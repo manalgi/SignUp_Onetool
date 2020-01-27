531414cf64c75244000aed59be6b48f8072cd924
manalgi <38757854+manalgi@users.noreply.github.com>
2020-01-27 03:32:18 +0100

    delete test.php

--- application/views/test.php
+++
@@ -1,5 +0,0 @@
-<?php
-$response = file_get_contents('https://api.kickbox.com/v2/verify?email=&apikey=');
-$response = json_decode($response, true);
-print_r($response["result"] == "deliverable");
-?>
